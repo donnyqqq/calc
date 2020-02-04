12e501c36875fd69e38b9ac2db8e5a8d4218eedb
Daniil Fomin <donatello.fomin@gmail.com>
2020-02-04 22:53:59 +0200

    start

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener('keydown', function (event) {
+  if (event.code == 'Enter') {
+    let calc = Array.from(document.getElementById("calc").value);
+    let calcNotNum = calc.filter(function (number) {
+      if (Number.isInteger(parseInt(number)) == false) {
+        return number;
+      }
+    });
+    let action = calcNotNum[0]
+    let num1Mass = []
+    for (let actionIndxSTPT = calc.indexOf(action) - 1; actionIndxSTPT != -1; actionIndxSTPT--) { 
+      num1Mass.push(calc[actionIndxSTPT])
+    }
+    let num1 = num1Mass.reverse().join('')
+    console.log(num1)
+    let num2Mass = []
+    for (let actionIndxSTPT = calc.indexOf(action) + 1; actionIndxSTPT != calc.length; actionIndxSTPT++) { 
+      num2Mass.push(calc[actionIndxSTPT])
+    }
+    let num2 = num2Mass.join('')
+    console.log(num2)
+    if(action == '-'){
+      alert(num1-num2)
+    }else if(action == '/'){
+      alert(num1/num2)
+    }else if(action == '+'){
+      alert(Number(num1)+Number(num2))
+    }else if(action == '*'){
+      alert(num1*num2)
+    }
+  }
+})
